c2f35b788a54aabae79d39797436aa4963e9e4bc
Doan Ngoc Long <linh_mieu_777@yahoo.com.vn>
2014-12-20 21:08:22 +0700

    update4_cmt_buttonPane

--- src/buttonPane.java
+++ src/buttonPane.java
@@ -1,54 +1,50 @@
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.image.BufferedImage;
-import java.io.IOException;
+import java.io.*;
+import java.awt.*;
+import java.awt.image.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.border.*;
 
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JPanel;
-import javax.swing.border.LineBorder;
-
-/**Class Panel chứa các button chỉnh sửa hình ảnh 
- *  mỗi nút nhận 1 nhiệm vụ riêng, các chức năng được gán trong Visualiseur 
- *  */
+/**
+ * Cette classe modélise un panneau contenant les boutons de traitement.
+ * Elle hérite de JPanel.
+ */
 public class buttonPane extends JPanel {
 		
-	/**Biến để canh vị trí các button */
+	/**
+	 * Variable servant à positionner les boutons par rapport à la fenêtre
+	 */
 	int position = 125;
 	
-	/**Panel hổ trợ để canh layout cho panel chính 
-	 * phải có cái panel này chêm đằng trước nếu không borderlayout sẽ làm
-	 * cái panel chính ép sát dí vào bên trái
-	 * */
+	/**
+	 * Un panneau secondaire aidant à positionner le panneau principal 
+	 */
 	JPanel sup = new JPanel();
 	
-	/**Panel chính chứa button */
+	/**
+	 * Un panneau principal  
+	 */
 	JPanelWithBackground but = new JPanelWithBackground("./img/button.jpg");
 	
-	/**Hàm dựng */
+	/**
+	 * Constructeur par défaut qui initialise le panneau des boutons
+	 */
 	public buttonPane() throws IOException {
 		super();		
-		but.setLayout(null);
-		// kẻ màu viền (border) cho panel
-		this.setBorder(new LineBorder(Color.red));
-		// điểu chỉnh kích thước
-		// sử dụng setPreferredSize để kích thước có thể tương thích với nhiều hệ máy khác nhau
+		but.setLayout(null);	
+		this.setBorder(new LineBorder(Color.red));		
 		this.setPreferredSize(new Dimension(830,130));
 		but.setPreferredSize(new Dimension(900,130));
 		but.setBorder(new LineBorder(Color.green));
 		sup.setPreferredSize(new Dimension(140,130));
-		// tùy chỉnh layout, ở đây sử dụng BorderLayout
+		
 		this.add(but,BorderLayout.CENTER);
 		this.add(sup,BorderLayout.CENTER);
 	}
 	
-	/**Hàm dùng đẻ thêm button theo vị trí mong muốn
-	 * nếu add thông thường vị trí sẽ không được đẹp
-	 * @param b
+	/**
+	 * Ajouter un nouveau bouton à une position désirée 
+	 * @param b Un nouveau bouton
 	 */
 	public void addBut(JButton b)
 	{
