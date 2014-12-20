d7210e834003f6d8176580126bb04d7447257915
Doan Ngoc Long <linh_mieu_777@yahoo.com.vn>
2014-12-20 20:07:36 +0700

    update4_cmt_mainPane

--- src/mainPane.java
+++ src/mainPane.java
@@ -1,66 +1,70 @@
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.Graphics;
-import java.awt.Image;
-import java.awt.Label;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
+import java.awt.*;
+import java.io.*;
+import javax.swing.*;
+import javax.swing.border.*;
 
-import javax.imageio.ImageIO;
-import javax.swing.ImageIcon;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.border.LineBorder;
-
-/**Class chứa-hiển thị hình ảnh cần chỉnh sửa (bên phải) và hình ảnh gốc (bên trái)
- * hình ảnh gốc sẽ không đổi để đảm bảo yêu cầu so sánh với hình ảnh được chỉnh sửa
- * @author doanngoclong
+/**
+ * Cette classe modélise les 2 panneaux contenant l'image source et l'image modifiée
+ * L'image source n'est jamais modifiée mais s'affiche quand même pour faire la comparaison des 2 images
  */
 public class mainPane extends JPanelWithBackground {
 	
-	/**Biến label chứa hình ảnh gốc*/
+	/**
+	 * Référence l'objet où est affichée l'image source
+	 */
 	private JLabel l_src = new JLabel();
 	
-	/**Biến label chứa hình ảnh đích*/
+	/**
+	 * Référence l'objet où est affichée l'image modifiée (destination)
+	 */
 	private JLabel l_dest = new JLabel();
 	
-	/**Biến Panel có background chứa hình ảnh gốc */
+	/**
+	 * Un panneau de l'image source avec une image de fond 
+	 */
 	JPanelWithBackground src_Pane = new JPanelWithBackground("./img/khunghinh.jpg");
 	
-	/**Biến Panel có background chứa hình ảnh đích */
+	/**
+	 * Un panneau de l'image modifiée avec une image de fond 
+	 */
 	JPanelWithBackground dest_Pane = new JPanelWithBackground("./img/khunghinh.jpg");
 	
-	/**Biến phụ Panel chứa ảnh gốc được đính kèm vào biến thanh cuộn src_scroll */
+	/**
+	 * Un panneau secondaire contenant l'image source attachée avec une barre de défilement src_scroll
+	 */
 	JPanel img_src_Pane = new JPanel();
 	
-	/**Biến phụ Panel chứa ảnh đích được đính kèm vào biến thanh cuộn dest_scroll */
+	/**
+	 * Un panneau secondaire contenant l'image modifiée attachée avec une barre de défilement dest_scroll
+	 */
 	JPanel img_dest_Pane = new JPanel();
 	
-	/**Biến thanh cuộn scroll chứa hình ảnh gốc, thanh cuộn sẽ xuất hiện khi
-	 * kích cỡ hình ảnh vượt ra khỏi khung Panel
+	/**
+	 * Référence l'objet de la barre de défilement contenant l'image source
 	 */
 	JScrollPane src_scroll = new JScrollPane(img_src_Pane);
 	
-	/**Biến thanh cuộn scroll chứa hình ảnh đích, thanh cuộn sẽ xuất hiện khi
-	 * kích cỡ hình ảnh vượt ra khỏi khung Panel
+	/**
+	 * Référence l'objet de la barre de défilement contenant l'image modifiée
 	 */
 	JScrollPane dest_scroll = new JScrollPane(img_dest_Pane);
 	
-	/**Hàm dựng với background*/
-	public mainPane() throws IOException {
+	/**
+	 * Constructeur par défaut du panneau avec une image de fond
+	 */
+	public mainPane() throws IOException 
+	{
 		super("./img/christmas.jpg");
 		src_Pane.setLayout(null);
 		dest_Pane.setLayout(null);
-		// thiết kế vị trí tuyệt đối (absolute position) cho panel chứa hình
+		
+		// Fixer la position absolue du panneau
 		src_scroll.setBounds(30,150,390,500);
 		dest_scroll.setBounds(30,150,390,500);
 		src_Pane.setPreferredSize(new Dimension(450,800));
 		dest_Pane.setPreferredSize(new Dimension(450,800));
-		// vẽ viền
+		
+		// Dessiner les bordures
 		this.setBorder(new LineBorder(Color.red));
 		src_Pane.setBorder(new LineBorder(Color.red));
 		dest_Pane.setBorder(new LineBorder(Color.red));
@@ -68,14 +72,18 @@ public class mainPane extends JPanelWithBackground {
 		dest_scroll.setBorder(new LineBorder(Color.green));
 		img_src_Pane.setBorder(new LineBorder(Color.blue));
 		img_dest_Pane.setBorder(new LineBorder(Color.blue));
+		
 		src_Pane.add(src_scroll);
 		dest_Pane.add(dest_scroll);
-		// thêm 2 panel chứa hình vào panel chính
+		
+		// Placer ces 2 panneaux dans le panneau principal
 		this.add(src_Pane,BorderLayout.CENTER);
 		this.add(dest_Pane,BorderLayout.CENTER);		
 	}
 
-	/**Hàm dùng để lấy truyền biến label chứa hình từ nơi khác */
+	/**
+	 * 
+	 */
 	public void setImage(JLabel img_src, JLabel img_dest)
 	{
 		l_src = img_src;
