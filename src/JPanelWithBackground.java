5235f8427f4c941611a0e306b20423ea0c19339f
Doan Ngoc Long <linh_mieu_777@yahoo.com.vn>
2014-12-20 20:16:27 +0700

    update4_cmt_JPanelWithBackground

--- src/JPanelWithBackground.java
+++ src/JPanelWithBackground.java
@@ -1,33 +1,32 @@
-import java.awt.Graphics;
-import java.awt.Image;
-import java.io.File;
-import java.io.IOException;
+import java.awt.*;
+import java.io.*;
+import javax.imageio.*;
+import javax.swing.*;
 
-import javax.imageio.ImageIO;
-import javax.swing.JPanel;
-
-/**Class Panel được vẽ background hình ảnh
- * có thể kế thừa class này thay vì JPanel để tạo background cho panel
- * */
+/**
+ * Cette classe modélise un panneau avec une image de fond
+ * Les autres classe peuvent hériter de cette classe pour avoir une image de fond
+ */
 public class JPanelWithBackground extends JPanel {
-
-	/**Biến hình background*/
+	
+	/**
+	 * Variable contenant l'image de fond
+	 */
 	private Image backgroundImage;
 	
-	/**Hàm dựng 
-	 * truyền biến đường dẫn file hình dùng để làm background
-	 * @param fileName
-	 * @throws IOException
+	/**
+	 * Constructeur par défaut prend en paramètre le chemin de l'image de fond
+	 * @param fileName le chemin de l'image de fond
 	 */
-	public JPanelWithBackground(String fileName) throws IOException {
+	public JPanelWithBackground(String fileName) throws IOException 
+	{
 		backgroundImage = ImageIO.read(new File((fileName)));
-	  }	
+	}		
 	
-	/**Hàm vẽ background sử dụng Graphics-drawImage */
-	public void paintComponent(Graphics g) {
-		super.paintComponent(g);
-	    	// vẽ hình nền lên Panel
-	    	g.drawImage(backgroundImage, 0, 0, this);
-	  }
+	public void paintComponent(Graphics g)
+	{
+		super.paintComponent(g);	    	
+	    g.drawImage(backgroundImage, 0, 0, this);
+	}
 
 }	
