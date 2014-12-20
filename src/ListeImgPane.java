0128a3987c2dfa48f1390cf99f1c698d802021dc
Doan Ngoc Long <linh_mieu_777@yahoo.com.vn>
2014-12-20 20:41:42 +0700

    update4_cmt_ListeImgPane

--- src/ListeImgPane.java
+++ src/ListeImgPane.java
@@ -9,35 +9,40 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.border.LineBorder;
 
-/**Class chứa list các hình ảnh được đưa vào chương trình để chỉnh sửa
- * tối đa lưu giữ được 10 hình, nếu quá sẽ hiện hộp thoại lỗi
- * các hình được vẽ thumbnail và đưa vào panel
- * @author doanngoclong
+/**
+ * Cette classe modélise la liste des images à traiter.
+ * Cette liste contient au maximum 10 images
  */
 public class ListeImgPane extends JPanelWithBackground {
 	
-	/**Biến panel chính để chứa hình */ 
+	/**
+	 * Référence l'objet où sont placés les images
+	 */ 
 	JPanel l_img_Pane = new JPanel();
 	
-	/**Biến scroll thanh cuộn */
+	/**
+	 * Référence l'objet qui initialise la barre de défilement
+	 */
 	JScrollPane scroll_Pane = new JScrollPane(l_img_Pane);
 	
-	/**Hàm dựng */
-	public ListeImgPane() throws IOException {
-		// dựng background hình kế thừa từ class JPanelWithBackground
+	/**
+	 * Constructeur par défaut qui initialise la liste des images à traiter
+	 */
+	public ListeImgPane() throws IOException 
+	{
 		super("./img/list.jpg");	
 		this.setLayout(null);
-		// kẻ viền
 		this.setBorder(new LineBorder(Color.red));
 		scroll_Pane.setBorder(new LineBorder(Color.red));
-		// tùy chỉnh kích thước
 		this.setPreferredSize(new Dimension(150,600));
-		scroll_Pane.setBounds(10,10,130,835);
-		l_img_Pane.setPreferredSize(new Dimension(110,1050));
+		scroll_Pane.setBounds(10,10,130,500);
+		l_img_Pane.setPreferredSize(new Dimension(110,500));
 		this.add(scroll_Pane);
 	}
 	
-	/**Hàm dùng để thêm các hình vào thanh cuộn của list hình */
+	/**
+	 * Ajouter les images à la barre de défilement
+	 */
 	public void addImgBut(JButton but_img)
 	{
 		l_img_Pane.add(but_img);
